30273785c971737688bca8d3b74ad62ae661fd99
kbk7525 <kbk7525@naver.com>
2021-11-15 11:20:06 +0900

    10866번 (덱)

---
+++ boj_10866.py
@@ -0,0 +1,38 @@
+#deque 라이브러리를 사용해서 해결
+import sys
+from collections import deque
+queue = deque()
+n = int(sys.stdin.readline())
+for _ in range(n):
+    order = sys.stdin.readline().split()
+    if order[0]=="push_front":
+        queue.appendleft(order[1])
+    elif order[0] == "push_back":
+        queue.append(order[1])
+    elif order[0] == "pop_front":
+        if len(queue)==0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif order[0] == "pop_back":
+        if len(queue)==0:
+            print(-1)
+        else:
+            print(queue.pop())
+    elif order[0] == "size":
+        print(len(queue)) 
+    elif order[0] == "empty":
+        if len(queue)==0:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == "front":
+        if len(queue)==0:
+             print(-1)   
+        else:
+            print(queue[0])
+    elif order[0] == "back":
+        if len(queue)==0:
+            print(-1)
+        else:
+            print(queue[-1])
